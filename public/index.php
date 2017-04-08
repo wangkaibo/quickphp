322ef3251988de17b808a1406e39189696068187
Kaibo Wang <wkaiboo@gmail.com>
2017-04-08 17:01:55 +0800

    Update index.php

--- public/index.php
+++ public/index.php
@@ -6,11 +6,6 @@
  * Time: 16:15
  */
 
-// 伪静态
-if (preg_match('/\.(?:png|jpg|jpeg|gif)$/', $_SERVER["REQUEST_URI"])) {
-	return false;    // serve the requested resource as-is.
-}
-
 require __DIR__ . '/../vendor/autoload.php';
 
 require_once  __DIR__ . '/../core/bootstrap.php';
